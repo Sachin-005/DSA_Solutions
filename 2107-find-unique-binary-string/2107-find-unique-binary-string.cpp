12faed1a0870e827d4d8a0049fd63b552bf2fadc
Sachin-005 <131667251+Sachin-005@users.noreply.github.com>
2025-02-21 21:32:50 +0530

    Time: 0 ms (100%), Space: 12.4 MB (94.31%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
@@ -1,30 +1,11 @@
 class Solution {
-    string res;
-
-    bool buildNumber(unordered_set<string>& numbers, string& curr, int& n) {
-        if (curr.size() == n) {
-            if (numbers.count(curr) == 0) {
-                res = curr;
-                return true;
-            }
-            return false;
-        }
-        curr.push_back('0');
-        if (buildNumber(numbers, curr, n)) return true;
-        curr.pop_back(); 
-        curr.push_back('1');
-        if (buildNumber(numbers, curr, n)) return true;
-        curr.pop_back();
-
-        return false;
-    }
-
 public:
     string findDifferentBinaryString(vector<string>& nums) {
-        unordered_set<string> numbers(nums.begin(), nums.end());
-        int n = nums[0].size(); 
-        string curr = "";
-        buildNumber(numbers, curr, n);
+        string res;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            res += (nums[i][i] == '0') ? '1' : '0'; // Flip diagonal elements
+        }
         return res;
     }
 };
