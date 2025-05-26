0aff2f8b68e0e9c2916f1836dfab3808de12b06f
Sachin K S <131667251+Sachin-005@users.noreply.github.com>
2025-05-26 18:01:52 +0530

    Time: 362 ms (5.85%), Space: 17.7 MB (57.23%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -4,11 +4,11 @@ public:
          int result = nums[0];
     for(int i=0;i<nums.size();i++) {
         int p = nums[i];
+         result = max(result,p);
         for(int j=i+1;j<nums.size();j++) {
+            p *= nums[j];
            result = max(result,p);
-           p *= nums[j];
         }
-        result = max(result,p);
     }
     return result;
     }
