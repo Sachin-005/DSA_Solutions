c904d8e79daf69ae9f0cf8a0fb15dc9e289f6f52
Sachin K S <131667251+Sachin-005@users.noreply.github.com>
2025-05-28 14:57:17 +0530

    Time: 0 ms (100%), Space: 17.9 MB (55.3%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,43 +1,30 @@
-			// \U0001f609\U0001f609\U0001f609\U0001f609Please upvote if it helps \U0001f609\U0001f609\U0001f609\U0001f609
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        int l = 0;
-        int r = nums.size() - 1;
-        
-        while(l <= r)
-        {
-            int mid = l + (r-l) / 2;
-            if (nums[mid] == target)
-                return true;
-			// with duplicates we can have this contdition, just update left & right
-            if((nums[l] == nums[mid]) && (nums[r] == nums[mid]))
-            {
-                l++;
-                r--;
+         int low =0;
+        int high = nums.size()-1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+             if(nums[mid] == target) return true;
+           else if((nums[low] == nums[mid]) && (nums[mid] == nums[high])){
+                low=low+1;
+                high = high-1;
             }
-			// first half
-			// first half is in order
-            else if(nums[l] <= nums[mid])
-            {
-				// target is in first  half
-                if((nums[l] <= target) && (nums[mid] > target))
-                    r = mid - 1;
+           
+            else if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && target<=nums[mid])
+                high = mid-1;
                 else
-                    l = mid + 1;
+                low = mid+1;
             }
-			// second half
-			// second half is order
-			// target is in second half
-            else
-            {
-                if((nums[mid] < target) && (nums[r]>= target))
-                    l = mid + 1;
+            else{
+                if(nums[mid]<=target && target<=nums[high])
+                low = mid+1;
                 else
-                    r = mid - 1;
+                high = mid-1;
             }
         }
-        return false;
+        return false;;
+    
     }
-	// for github repository link go to my profile.
 };
