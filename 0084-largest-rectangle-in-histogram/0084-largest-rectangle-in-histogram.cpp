4b8dbe9968f691dde37b5f6e9483854008c10101
Sachin K S <sachinsenthilvel2005@gmail.com>
2025-07-14 09:59:44 +0530

    Time: 20 ms (68.81%), Space: 85.8 MB (38.16%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -3,20 +3,18 @@ private:
     void nextSmall(vector<int>& heights, vector<int>& nse) {
         stack<int> st;
         int n = heights.size();
-       // nse.resize(n, n);  // Initialize nse with n to indicate no smaller element
+        nse.resize(n, n);  
 
         for (int i = n - 1; i >= 0; i--) {
-            // Pop elements from stack while the stack is not empty and the height of the element at index on the stack is greater than or equal to heights[i]
+           
             while (!st.empty() && heights[i] <= heights[st.top()]) {
                 st.pop();
             }
 
-            // If stack is not empty, the next smaller element index is the top of the stack
+           
             if (!st.empty()) {
                 nse[i] = st.top();
             }
-
-            // Push current index to stack
             st.push(i);
         }
     }
@@ -24,20 +22,16 @@ private:
     void previousSmall(vector<int>& heights, vector<int>& pse) {
         stack<int> st;
         int n = heights.size();
-     //  pse.resize(n, -1);  // Initialize pse with -1 to indicate no smaller element
+        pse.resize(n, -1);  
 
         for (int i = 0; i < n; i++) {
-            // Pop elements from stack while the stack is not empty and the height of the element at index on the stack is greater than or equal to heights[i]
+           
             while (!st.empty() && heights[i] <= heights[st.top()]) {
                 st.pop();
             }
-
-            // If stack is not empty, the previous smaller element index is the top of the stack
             if (!st.empty()) {
                 pse[i] = st.top();
             }
-
-            // Push current index to stack
            st.push(i);
         }
     }
@@ -47,18 +41,13 @@ public:
         int n = heights.size();
         if (n == 0) return 0;
 
-        vector<int> nse(n,n);  // Next smaller elements indices
-        vector<int> pse(n,-1);  // Previous smaller elements indices
-
-        // Find next smaller elements
+        vector<int> nse; 
+        vector<int> pse;  
         nextSmall(heights, nse);
-
-        // Find previous smaller elements
         previousSmall(heights, pse);
 
         int maxArea = 0;
 
-        // Calculate the area for each bar in the histogram
         for (int i = 0; i < n; i++) {
             int width = nse[i] - pse[i] - 1;
             int area = heights[i] * width;
