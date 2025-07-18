3121962fab26dac702d85b3895ee3db8a37af08f
Sachin K S <sachinsenthilvel2005@gmail.com>
2025-07-18 18:40:25 +0530

    Time: 6 ms (48.03%), Space: 9.7 MB (84.6%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.cpp
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -6,18 +6,13 @@ public:
         
         string repeated = a;
         int count = 1;
-        
-        // Repeat until length >= length of b
+    
         while (repeated.length() < nb) {
             repeated += a;
             count++;
         }
-        
-        // Check if b is now a substring
         if (repeated.find(b) != string::npos)
             return count;
-
-        // Try one more repeat
         repeated += a;
         count++;
 
